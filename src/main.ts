195a9050064e8b6d6c427cdbb9a5b7fae18f8abc
FajlaRabby24 <fajlarabbyla24@gmail.com>
2026-09-15 15:40:23 +0600

    feat: add application bootstrap with Swagger documentation and bundler configurations

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module.js';
+
+// declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +16,21 @@ async function bootstrap() {
   // const seedService = app.get(SeedService);
   // await seedService.seed();
 
+  const config = new DocumentBuilder()
+    .setTitle('Spotify-clone')
+    .setDescription('The Spotify clone api documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(configService.getOrThrow('PORT'));
+
+  // if (module.hot) {
+  //   module.hot.accept();
+  //   module.hot.dispose(() => app.close());
+  // }
+  // console.log('hello');
 }
 await bootstrap();
