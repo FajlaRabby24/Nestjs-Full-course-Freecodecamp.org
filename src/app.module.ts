8dc9f85a01c641649ba7ff15734c45a9abf44989
FajlaRabby24 <fajlarabbyla24@gmail.com>
2026-09-17 11:49:53 +0600

    feat: integrate Mongoose and Config modules with Song schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        `${process.env.NODE_ENV}.env`,
+        '.env.development',
+        '.env.production',
+        '.env.local',
+        '.env',
+      ],
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI!),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
